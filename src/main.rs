1d5e5e077174621d085f58d2a5013393a18ba724
subh <subhajitchaudhury05@gmail.com>
2026-03-20 15:10:51 +0530

    feat : added required files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+// // main.rs
+// mod cheney;
+// mod common;
+// mod generational;
+// mod mark_sweep;
+// mod ref_count;
+// mod tracing;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Garbage Collector");
 }
